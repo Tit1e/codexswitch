8bd18fb899116e30bfc5b4b93dbe2b54f7b18f18
chenhaoliang <evollyone@outlook.com>
2026-03-18 10:04:00 +0800

    Initial Commit

---
+++ codexswitch/codexswitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  codexswitchApp.swift
+//  codexswitch
+//
+//  Created by 陈浩亮 on 2026/3/18.
+//
+
+import SwiftUI
+
+@main
+struct codexswitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
